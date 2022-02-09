4846ec3ffa98e2aea3f38b30717e4bb409f4592f
Lois Hamilton <2547511h@student.gla.ac.uk>
2022-02-09 20:35:20 +0000

    first commit

--- rango/admin.py
+++ rango/admin.py
@@ -3,7 +3,11 @@ from rango.models import Category, Page
 
 class PageAdmin(admin.ModelAdmin):
 	list_display = ('title', 'category', 'url')
+	
+class CategoryAdmin(admin.ModelAdmin):
+	list_display = ('name', 'views', 'likes')
+	prepopulated_fields = {'slug':('name',)}
 
 
-admin.site.register(Category)
+admin.site.register(Category, CategoryAdmin)
 admin.site.register(Page, PageAdmin)
