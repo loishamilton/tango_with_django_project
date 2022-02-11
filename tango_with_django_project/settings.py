14e5d68f41c8193b668866983d59a32250a6846b
Lois Hamilton <2547511h@student.gla.ac.uk>
2022-02-11 17:24:34 +0000

    first commit

--- tango_with_django_project/settings.py
+++ tango_with_django_project/settings.py
@@ -105,6 +105,12 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
+PASSWORD_HASHERS = [
+	'django.contrib.auth.hashers.BCryptSHA256PasswordHasher',
+	'django.contrib.auth.hashers.BCryptPasswordHasher',	
+	'django.contrib.auth.hashers.PBKDF2PasswordHasher',
+	'django.contrib.auth.hashers.PBKDF2SHA1PasswordHasher',
+]
 
 # Internationalization
 # https://docs.djangoproject.com/en/2.2/topics/i18n/
@@ -118,6 +124,8 @@ USE_I18N = True
 USE_L10N = True
 
 USE_TZ = True
+
+LOGIN_URL = 'rango:login'
 
 
 # Static files (CSS, JavaScript, Images)
