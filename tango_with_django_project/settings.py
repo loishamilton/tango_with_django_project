e64c9374aaefac054fada2ec4b9a6f187b944065
Lois Hamilton <2547511h@student.gla.ac.uk>
2022-02-11 18:13:00 +0000

    first commit

--- tango_with_django_project/settings.py
+++ tango_with_django_project/settings.py
@@ -127,6 +127,10 @@ USE_TZ = True
 
 LOGIN_URL = 'rango:login'
 
+SESSION_EXPIRE_AT_BROWSER_CLOSE = True
+
+SESSION_COOKIE_AGE = 1209600
+
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
