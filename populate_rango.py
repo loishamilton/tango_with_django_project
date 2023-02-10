44670ea2a87f8be676b3a8262090792fdb0f454b
Lois Hamilton <2547511h@student.gla.ac.uk>
2023-02-10 03:12:32 +0000

    chapter five

--- populate_rango.py
+++ populate_rango.py
@@ -36,7 +36,7 @@ def populate():
 			
 			
 	for cat, cat_data in cats.items():
-		c = add_cat(cat)
+		c = add_cat(cat, views=cat_data['views'], likes=cat_data['likes'])
 		for p in cat_data['pages']:
 			add_page(c, p['title'], p['url'])
 
